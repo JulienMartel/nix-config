e02ccebbcefcfaacff7d8554cdc756bbc691508e
Julien Martel <julienbmartel@gmail.com>
2026-07-29 11:34:51 -0500

    done test

--- hosts/mbp/packages/nixpkgs-htop.nix
+++
@@ -1,8 +0,0 @@
-# Added by pounce "Install App". Safe to edit or remove.
-{ lib, pkgs, ... }:
-{
-  environment.systemPackages = [
-    (lib.attrByPath (lib.splitString "." "htop")
-      (throw ("pounce Install App: Nixpkgs package " + "htop" + " does not exist")) pkgs)
-  ];
-}
