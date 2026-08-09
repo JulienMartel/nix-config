77e4b9b8ff90b4dbfa1b02326f44babbf889543f
Julien Martel <julienbmartel@gmail.com>
2026-08-09 07:38:59 -0500

    name change

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   # The whole rice (system + shell + pounce + nebelung) comes from the public
   # nebelhaus flake. This private config holds only what's personal: the host.
   # Update everything with:  nix flake update nebelhaus
-  inputs.nebelhaus.url = "github:nebelhaus/nebelhaus";
+  inputs.nebelhaus.url = "github:hausfold/hausfold";
 
   outputs =
     { nebelhaus, ... }:
