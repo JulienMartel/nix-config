a8ef2b4e32f8937544fa1c8dbfa0ed31735184cc
Julien Martel <julienbmartel@gmail.com>
2026-08-15 00:55:15 -0500

    default media pill width

--- hosts/mbp/settings/sill.media.width.nix
+++
@@ -1,7 +0,0 @@
-# Managed by haus set. Ordinary Nix: safe to inspect or edit.
-# Remove this override with: haus reset sill.media.width
-{ lib, ... }:
-
-{
-  haus.sill.media.width = lib.mkForce (builtins.fromJSON "90");
-}
